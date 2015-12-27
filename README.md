30cd4f08b6baa509d50e01e02f10b3442ace0391
Justin Holguin <justin.holguin@puppetlabs.com>
2015-12-26 20:22:26 -0800

    Init.

---
+++ README.md
@@ -0,0 +1,43 @@
+# idris-new.sh
+
+Create a new, slightly customized, mostly blank [Idris](http://idris-lang.org) project to use as a starting point for your application or library.
+
+## requirements
+
+Running the script doesn't require anything but Bash 4.0 or greater, which you should have installed already if you're not on OS X.
+If you are, you may want to install a more recent version of Bash using [homebrew](http://brew.sh).
+
+## usage
+
+Put the `idris-new.sh` script somewhere on your path and make sure it's executable. Unless you're on Windows, that will probably look a little something like this:
+
+```
+cp idris-new.sh /usr/local/bin/idris-new
+chmod +x /usr/local/bin/idris-new
+```
+
+Then call it with the name of your project as the first and only argument.
+The name should consist of only alphanumeric characters and start with a lowercase letter. For example:
+
+```
+idris-new myCoolProject
+```
+
+Feel free to try out your project by typing `make` in the new directory, then running the new `myCoolProject` executable.
+
+## what it does
+
+The command above will do the following:
+
+* create a new `myCoolProject` directory
+* create a reasonably common project structure with a couple of modules that amount to little more than a classic "Hello, world" example
+* create `myCoolProject.ipkg` and `Makefile` so that you can immediately build an executable with the `make` command
+* create a small readme
+* add a basic `.gitignore` file
+* add a `.dir-locals.el` file so that Emacs will load the right libraries when it starts the Idris REPL
+
+## what it doesn't do
+
+* initialize the git repo. Your choice of VCS is up to you.
+* add a LICENSE or CONTRIBUTING document, because those are also entirely up to you.
+* clean up your project name if it has hyphens or emoji or whatever. Don't do that.
---
+++ idris-new.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env bash
+
+# Version 0.0.1
+
+set -euo pipefail
+IFS=$'\n\t'
+
+PROGRAM_NAME=$1
+MODULE_NAME="${PROGRAM_NAME^}" # might fail on Bash < 4.0
+MAIN_FILE="${MODULE_NAME}.idr"
+IPKG_FILE="${PROGRAM_NAME}.ipkg"
+
+echo "Creating a directory for ${PROGRAM_NAME}."
+mkdir $PROGRAM_NAME
+
+echo "Entering the ${PROGRAM_NAME} directory."
+cd $PROGRAM_NAME
+
+echo "Creating the src directory."
+mkdir src
+
+echo "Creating the src/${MODULE_NAME} directory."
+mkdir src/${MODULE_NAME}
+
+echo "Creating src/${MODULE_NAME}/Actions.idr from the 'Hello, world' template."
+cat <<-HERE > src/${MODULE_NAME}/Actions.idr
+module ${MODULE_NAME}.Actions
+
+import Effects
+import Effect.StdIO
+
+||| Defining this in another file using the STDIO effect is not that practical,
+||| but it should help illustrate the project structure.
+sayHello : Eff () [STDIO]
+sayHello = putStrLn "Hello, world!"
+HERE
+
+echo "Creating src/${MAIN_FILE} from 'Hello, world' template."
+cat <<-HERE > src/$MAIN_FILE
+module Main
+
+import Effects
+import Effect.StdIO
+
+import ${MODULE_NAME}.Actions -- defines \`sayHello\`
+
+main : IO ()
+main = run sayHello
+HERE
+
+echo "Creating the ${PROGRAM_NAME}.ipkg file."
+cat <<- HERE > $IPKG_FILE
+package ${PROGRAM_NAME}
+
+modules = ${MODULE_NAME}, ${MODULE_NAME}.Actions
+sourcedir = src
+
+opts = "-p effects"
+
+main = ${MODULE_NAME}
+executable = ${PROGRAM_NAME}
+HERE
+
+echo "Creating the Makefile."
+cat << HERE > Makefile
+DEFAULT:
+	idris --build ${IPKG_FILE}
+
+clean:
+	idris --clean ${IPKG_FILE}
+HERE
+
+echo "Creating a basic .gitignore file."
+cat <<-HERE > .gitignore
+*.ibc
+/${PROGRAM_NAME}
+HERE
+
+echo "Creating the Emacs .dir-locals.el file."
+cat <<-HERE > .dir-locals.el
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((idris-mode
+  (idris-load-packages .
+                       ("effects"))))
+HERE
+
+echo "Creating a basic README.md file."
+cat <<-HERE > README.md
+# ${PROGRAM_NAME}
+
+What this program does is up to you!
+
+## building
+
+Run \`make\` from this directory to generate the ${PROGRAM_NAME} executable.
+
+## adding dependencies
+
+Once the library you'd like to use is installed, you can add it to the \`opts\` line in ${IPKG_FILE}. Remember to include a leading \`-p\` for each library, so adding the \`contrib\` library would result in:
+\`\`\`
+opts = "-p effects -p contrib"
+\`\`\`
+
+If you use [idris-mode](https://github.com/idris-hackers/idris-mode) for Emacs, then you'll also want to add the library name to .dir-locals.el so that it will be loaded with the REPL process.
+HERE
