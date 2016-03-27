dcc9664f855db8071204b51161ab74135486605f
Justin Holguin <justin.h.holguin@gmail.com>
2016-03-27 12:18:30 -0700

    Update for Idris 0.11
    The recent access changes (making all bindings private by default) broke
    the template, so the `sayHello` function is now `public export`ed.

--- idris-new.sh
+++ idris-new.sh
@@ -31,6 +31,7 @@ import Effect.StdIO
 
 ||| Defining this in another file using the STDIO effect is not that practical,
 ||| but it should help illustrate the project structure.
+public export
 sayHello : Eff () [STDIO]
 sayHello = putStrLn "Hello, world!"
 HERE
